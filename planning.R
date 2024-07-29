ac183b7f2b3e186fd97f5e98a8035da67d4af9f1
rstruchen <rahel.struchen@blv.admin.ch>
2024-07-29 16:43:21 +0200

    update file paths

--- planning.R
+++ planning.R
@@ -1,14 +1,14 @@
 
-source('O:/1_Abteilungen/111_TG/UTG/111.4_UTG_ÜP/111.42_SF/ÜP 2022/Resultate/IBR_EBL/gary_AIND/IBREBL/code/scripts/01_planning_01_packages.R', encoding = 'UTF-8')
+source('code/01_planning_01_packages.R', encoding = 'UTF-8')
 
-source('O:/1_Abteilungen/111_TG/UTG/111.4_UTG_ÜP/111.42_SF/ÜP 2022/Resultate/IBR_EBL/gary_AIND/IBREBL/code/scripts/01_planning_02_loadData.R', encoding = 'UTF-8')
+source('code/01_planning_02_loadData.R', encoding = 'UTF-8')
 
-source('O:/1_Abteilungen/111_TG/UTG/111.4_UTG_ÜP/111.42_SF/ÜP 2022/Resultate/IBR_EBL/gary_AIND/IBREBL/code/scripts/01_planning_03_editData.R', encoding = 'UTF-8')
+source('code/01_planning_03_editData.R', encoding = 'UTF-8')
 
-source('O:/1_Abteilungen/111_TG/UTG/111.4_UTG_ÜP/111.42_SF/ÜP 2022/Resultate/IBR_EBL/gary_AIND/IBREBL/code/scripts/01_planning_04_calculationsPartI.R', encoding = 'UTF-8')
+source('code/01_planning_04_calculationsPartI.R', encoding = 'UTF-8')
 
-source('O:/1_Abteilungen/111_TG/UTG/111.4_UTG_ÜP/111.42_SF/ÜP 2022/Resultate/IBR_EBL/gary_AIND/IBREBL/code/scripts/01_planning_05_exportIntermResults.R', encoding = 'UTF-8')
+source('code/01_planning_05_exportIntermResults.R', encoding = 'UTF-8')
 
-source('O:/1_Abteilungen/111_TG/UTG/111.4_UTG_ÜP/111.42_SF/ÜP 2022/Resultate/IBR_EBL/gary_AIND/IBREBL/code/scripts/01_planning_06_calculationsPartII.R', encoding = 'UTF-8')
+source('code/01_planning_06_calculationsPartII.R', encoding = 'UTF-8')
 
-source('O:/1_Abteilungen/111_TG/UTG/111.4_UTG_ÜP/111.42_SF/ÜP 2022/Resultate/IBR_EBL/gary_AIND/IBREBL/code/scripts/01_planning_07_exportFinalResults.R', encoding = 'UTF-8')
+source('code/01_planning_07_exportFinalResults.R', encoding = 'UTF-8')
